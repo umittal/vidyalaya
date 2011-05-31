d45260eb740e51b394f41c9650efb8e7344ccd8b
Umesh Mittal <umesh.mittal@qvt.com>
2011-05-31 10:54:33 -0400

    viewer updates

--- dakhila/php/dataViewer.php
+++ dakhila/php/dataViewer.php
@@ -65,16 +65,41 @@ switch ($command) {
 		break;
 
 	case "RegistrationSummary":
-		$sql = "select previousYear, currentYear, count(*) from FamilyTracker where year = 1 group by previousYear, currentYear";
+		$sql = <<< SQLREGISTRATIONSUMMAY
+		  select previousYear, currentYear, count(*) 
+		  from FamilyTracker where year = 1 
+		  group by previousYear, currentYear 
+		  order by previousYear, currentYear
+
+SQLREGISTRATIONSUMMAY;
 		$result = VidDb::query($sql);
-		print "<html><body><table>\n";
+		$subTotal = 0; $total = 0; $current = null;
+		print "<html><body><table width='400'>\n";
+		print "<tr><th>Current Year</th><th>Next Year</th><th>Count</th></tr>\n";
 		while ($row = mysql_fetch_array($result)) {
+		  $currentYear = $row[0];
+		  if ($current == null) $current = $currentYear;
+		  if ($current !=  $currentYear) {
+		    //show subtotal
+			print "<tr><td colspan=2>";
+			print "Subtotal </td><td align=right> " . $subTotal;
+			print "</td></tr>\n<tr><td>&nbsp;</td></tr>\n";
+			$current = $currentYear;
+			$subTotal = 0;
+		  }
 			print "<tr><td>";
 			print EnumFamilyTracker::NameFromId($row[0]) . "</td><td> " . EnumFamilyTracker::NameFromId($row[1]) .
-			"</td><td> " . $row[2] ;
-
+			"</td><td align=right> " . $row[2] ;
 			print "</td></tr>\n";
+			$subTotal += $row[2];
+			$total += $row[2];
 		}
+			print "<tr><td colspan=2>";
+			print "Subtotal </td><td align=right> " . $subTotal;
+			print "</td></tr>\n<tr><td>&nbsp;</td></tr>\n";
+			print "<tr><td colspan=2>";
+			print "Total </td><td align=right> " . $total;
+			print "</td></tr>\n";
 		print "\n</table></body></html>";		
 		break;
 
