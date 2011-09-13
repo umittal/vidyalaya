1cd57d194b8bbbdfb242acd69c7180cc06be00f5
Umesh Mittal <umesh@vidyalaya.us>
2011-09-13 16:43:08 -0400

    make a local copy of the database from backup

---
+++ bash/vidyalaya.bash
@@ -0,0 +1,2 @@
+mysqldump --host=vidyalaya.db.4718809.hostedresource.com --user=vidyalaya --password=Praveen38 --add-drop-table vidyalaya > dump
+mysql --user=vidyalaya --password=Praveen38 vidyalaya < dump
